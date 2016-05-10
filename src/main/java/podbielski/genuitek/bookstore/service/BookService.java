cb8167b53d5290d0f5ca6bea5d1b116eb3cf1fda
jfonferko <jfonferko@genuitek.com>
2016-05-10 10:30:20 +0200

    JFO - implemntacja findAllOlderThanYear

--- src/main/java/podbielski/genuitek/bookstore/service/BookService.java
+++ src/main/java/podbielski/genuitek/bookstore/service/BookService.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package podbielski.genuitek.bookstore.service;
 
@@ -9,9 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import podbielski.genuitek.bookstore.domain.Book;
 import podbielski.genuitek.bookstore.repository.BookRepository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author mateusz.podbielski@genuitek.com
@@ -20,23 +18,15 @@ import java.util.Map;
 
 @Service
 @Transactional
-public class BookService
-{
-	@Autowired private BookRepository bookRepository;
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
 
-	public List<Book> findAllBooks() {
-		return bookRepository.findAll();
-	}
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
 
-	//TODO JFO implmentacja
-	public List<Book> findAllOlderThanYear(int year) {
-//		//FIXME JFO Zła implementacja - nalezy skorzystac z repository i __NIE_ITEROWAC__ obiektow
-//		List<Book> books = new ArrayList<Book>();
-//		for(Book book : this.bookRepository.findAll()){
-//			if(book.getYear() < year){
-//				books.add(book);
-//			}
-//		}
-		return null;
-	}
+    public List<Book> findAllOlderThanYear(int year) {
+        return bookRepository.findOlderThanYear(year);
+    }
 }
